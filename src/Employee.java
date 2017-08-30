00b847df25bc019522b4c1394efb3fd7e8708630
welcome <welcome@DESKTOP-0GTLG5R>
2017-08-30 20:50:42 +0530

    commit by dv

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+
+public class Employee {
+ private int empid;
+ private String ename;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+ 
+}
